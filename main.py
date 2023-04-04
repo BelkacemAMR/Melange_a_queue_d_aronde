7499d478513396e9ef95ad9427cd46a6986fb874
Belkacem <abel-and-co@hotmail.com>
2023-04-04 11:36:30 +0200

    Ajout du test de la fonction

--- main.py
+++ main.py
@@ -11,3 +11,19 @@ def perfect_shuffle(deck):
         mix.append(b[i])
 
     return mix
+
+# On teste :
+
+# Créer une liste de cartes ou de nombres
+
+deck = [1, 2, 3, 4, 5, 6, 7, 8]
+
+# Appeler la fonction perfect_shuffle()
+
+shuffled_deck = perfect_shuffle(deck)
+
+# Vérifier si le résultat est correct
+
+print(shuffled_deck)
+
+# Résultat : [1, 5, 2, 6, 3, 7, 4, 8]
